ff1ab8cde446fa51f204b06c55e22b6bcbb11b13
bekabeshka <4bekdaulet@gmail.com>
2023-12-07 23:37:30 +0600

    Validating the UI with Snapshot Tests + Dark Mode Support

---
+++ EssentialFeed/EssentialFeediOS/Feed UI/Views/Helpers/UITableView+HeaderSizing.swift
@@ -0,0 +1,21 @@
+//
+//  UITableView+HeaderSizing.swift
+//  EssentialFeediOS
+//
+//  Created by bekabeshka on 07.12.2023.
+//  Copyright © 2023 Essential Developer. All rights reserved.
+//
+
+import UIKit
+
+extension UITableView {
+    func sizeTableHeaderToFit () {
+        guard let header = tableHeaderView else { return }
+        let size = header.systemLayoutSizeFitting (UIView.layoutFittingCompressedSize)
+        let needsFrameUpdate = header.frame.height != size.height
+        if needsFrameUpdate {
+            header.frame.size.height = size.height
+            tableHeaderView = header
+        }
+    }
+}
